c2fcafb64452bc7010bf6c7243494a3a1ab764d4
webdevj1 <zgsales@aol.com>
2018-08-19 08:17:37 -0400

    08_18_18: reverse a string not using a for loop, "updated"

--- 08_18_18.js
+++ 08_18_18.js
@@ -1,10 +1,10 @@
 //'reverse a string not using a for loop'
 
 function revString(string){
-    return string.split("").reverse().join('');
-    // 1) string.split("")  = [ 'j', 'a', 'm', 'e', 's' ]
-    // 2) .reverse()        = [ 's', 'e', 'm', 'a', 'j' ]
-    // 3) .join()           = semaj
+    return string.split('').reverse().join('');
+    // 1) string.split('')  = [ 'j', 'a', 'm', 'e', 's' ]   (split each letter of string into array)
+    // 2) .reverse()        = [ 's', 'e', 'm', 'a', 'j' ]   (.reverse() method)
+    // 3) .join('')           = semaj                       (join array into string with out commas)
 }
 
 revString('james'); // 'semaj'
