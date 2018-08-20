dc9ef226fb83d5941b1bd5a4df61b069a385e739
webdevj1 <zgsales@aol.com>
2018-08-20 10:18:14 -0400

    08_20_18: Take a string as an input and return the string with the letters in alphabetical order- code solution

--- 08_20_18.js
+++ 08_20_18.js
@@ -5,8 +5,11 @@ Take a string as an input and return the string with the letters in alphabetical
 
 
 function sortLetters(string){
+    return string.split('').sort().join('');
         // 1) use .split('') to go from string to array
         // 2) .sort() the letters in array
         // 3) .join('') to go from array back to string
 
 }
+
+console.log(sortLetters('monday'));
