eb2616b6925f359efa5a68fa13419bfc1ff7fd48
webdevj1 <zgsales@aol.com>
2018-08-20 10:14:22 -0400

    08_20_18: Take a string as an input and return the string with the letters in alphabetical order

---
+++ 08_20_18.js
@@ -0,0 +1,12 @@
+/*
+Take a string as an input and return the string with the letters in alphabetical order
+*/
+
+
+
+function sortLetters(string){
+        // 1) use .split('') to go from string to array
+        // 2) .sort() the letters in array
+        // 3) .join('') to go from array back to string
+
+}
