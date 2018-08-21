5b9bf4e5f95a3109bd7780a7a2788dd18093f3e9
webdevj1 <zgsales@aol.com>
2018-08-21 17:21:29 -0400

    08_21_18: Take a array and a number as an input and return the array rotated the number of times given.

---
+++ 08_21_18.js
@@ -0,0 +1,17 @@
+/*
+08_21_18: Take a array and a number as an input and return the array rotated the number of times given.
+*/
+
+function row2(array, num){
+    var count = 1;
+  
+    for(i=num; i>0; i--){
+      var poppedIndex = array.pop();
+      console.log(`rotation ${count} = "${poppedIndex}"` )
+      array.unshift(poppedIndex);
+      count++;
+    }
+    return array;
+  }
+  
+  row2([1,2,3,4],2); // => [ 3, 4, 1, 2 ]
